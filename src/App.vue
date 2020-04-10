f37c6548f7b6f980ff435064f66f77ecf333e0df
Water.Li <11011968@qq.com>
2020-04-10 16:55:43 +0800

    添加抽屉迷你显示

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,144 @@
         v-model="primaryDrawer.model"
         :clipped="primaryDrawer.clipped"
         :floating="primaryDrawer.floating"
-        :mini-variant="primaryDrawer.mini"
+        :mini-variant.sync="primaryDrawer.mini"
         :permanent="primaryDrawer.type === 'permanent'"
         :temporary="primaryDrawer.type === 'temporary'"
         app
         overflow
+
       >
         <v-toolbar color="primary darken-1" dark>
-          <img src="images/logo.png" height="36" alt="Vular Amazing Framework" />
+          <img src="images/logo.png" :height="primaryDrawer.mini ? 26 : 36" alt="Vular Amazing Framework" />
           <v-toolbar-title class="ml-0 pl-3">
-            <span class="hidden-sm-and-down">Vue Material</span>
+            <span class="hidden-sm-and-down">Vular</span>
           </v-toolbar-title>
           <v-spacer></v-spacer>
-          <v-btn icon class="hidden-xs-only" >
-            <v-icon>chevron_right</v-icon>
+          <v-btn icon class="hidden-xs-only" 
+          @click.stop="primaryDrawer.mini = !primaryDrawer.mini"
+          >
+            <v-icon>chevron_left</v-icon>
           </v-btn>
         </v-toolbar>
-        <v-list dense>
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Home</v-list-item-title>
-            </v-list-item-content>
+        <v-list>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>dashboard</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-title>仪表盘</v-list-item-title>
           </v-list-item>
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon>mdi-contact-mail</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Contact</v-list-item-title>
-            </v-list-item-content>
+
+          <v-list-group
+            prepend-icon="account_circle"
+            value="true"
+          >
+            <template v-slot:activator>
+              <v-list-item-title>Users</v-list-item-title>
+            </template>
+
+            <v-list-group
+              no-action
+              sub-group
+              value="true"
+            >
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>Admin</v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item
+                v-for="(admin, i) in admins"
+                :key="i"
+                link
+              >
+                <v-list-item-title v-text="admin[0]"></v-list-item-title>
+                <v-list-item-icon>
+                  <v-icon v-text="admin[1]"></v-icon>
+                </v-list-item-icon>
+              </v-list-item>
+            </v-list-group>
+
+            <v-list-group
+              sub-group
+              no-action
+            >
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>Actions</v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <v-list-item
+                v-for="(crud, i) in cruds"
+                :key="i"
+                @click=""
+              >
+                <v-list-item-title v-text="crud[0]"></v-list-item-title>
+                <v-list-item-action>
+                  <v-icon v-text="crud[1]"></v-icon>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list-group>
+          </v-list-group>
+          <v-list-group
+            prepend-icon="account_circle"
+            value="true"
+          >
+            <template v-slot:activator>
+              <v-list-item-title>Users</v-list-item-title>
+            </template>
+
+            <v-list-group
+              no-action
+              sub-group
+              value="true"
+            >
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>Admin</v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item
+                v-for="(admin, i) in admins"
+                :key="i"
+                link
+              >
+                <v-list-item-title v-text="admin[0]"></v-list-item-title>
+                <v-list-item-icon>
+                  <v-icon v-text="admin[1]"></v-icon>
+                </v-list-item-icon>
+              </v-list-item>
+            </v-list-group>
+
+            <v-list-group
+              sub-group
+              no-action
+            >
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>Actions</v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <v-list-item
+                v-for="(crud, i) in cruds"
+                :key="i"
+                @click=""
+              >
+                <v-list-item-title v-text="crud[0]"></v-list-item-title>
+                <v-list-item-action>
+                  <v-icon v-text="crud[1]"></v-icon>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list-group>
+          </v-list-group>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>settings</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-title>设置</v-list-item-title>
           </v-list-item>
         </v-list>
 
@@ -52,7 +158,7 @@
           v-if="primaryDrawer.type !== 'permanent'"
           @click.stop="primaryDrawer.model = !primaryDrawer.model"
         />
-        <v-toolbar-title>Vuetify</v-toolbar-title>
+        <v-toolbar-title></v-toolbar-title>
       </v-app-bar>
 
       <v-content>
@@ -150,6 +256,16 @@
 export default {
   name: 'app',
   data: () => ({
+      admins: [
+        ['Management', 'people_outline'],
+        ['Settings', 'settings'],
+      ],
+      cruds: [
+        ['Create', 'add'],
+        ['Read', 'insert_drive_file'],
+        ['Update', 'update'],
+        ['Delete', 'delete'],
+      ],
       drawers: ['Default (no property)', 'Permanent', 'Temporary'],
       primaryDrawer: {
         model: null,
@@ -167,12 +283,6 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
 h1, h2 {
