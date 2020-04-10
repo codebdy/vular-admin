00088c00b0510d25dea9f25d37755b4bbb828391
Water.Li <11011968@qq.com>
2020-04-10 20:50:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -12,140 +12,110 @@
         overflow
 
       >
-        <v-toolbar color="primary darken-1" dark>
-          <img src="images/logo.png" :height="primaryDrawer.mini ? 26 : 36" alt="Vular Amazing Framework" />
-          <v-toolbar-title class="ml-0 pl-3">
-            <span class="hidden-sm-and-down">Vular</span>
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon class="hidden-xs-only" 
-          @click.stop="primaryDrawer.mini = !primaryDrawer.mini"
-          >
-            <v-icon>chevron_left</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <v-list>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>dashboard</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-title>仪表盘</v-list-item-title>
-          </v-list-item>
-
-          <v-list-group
-            prepend-icon="account_circle"
-            value="true"
-          >
-            <template v-slot:activator>
-              <v-list-item-title>Users</v-list-item-title>
-            </template>
-
-            <v-list-group
-              no-action
-              sub-group
-              value="true"
+      <div style="width: 100%;height: 100%;display: flex;flex-flow: column;">
+          <v-toolbar color="primary darken-1" dark style="flex: 0">
+            <img src="images/logo.png" :height="primaryDrawer.mini ? 26 : 36" alt="Vular Amazing Framework" />
+            <v-toolbar-title class="ml-0 pl-3">
+              <span class="hidden-sm-and-down">Vular</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon class="hidden-xs-only" 
+            @click.stop="primaryDrawer.mini = !primaryDrawer.mini"
             >
-              <template v-slot:activator>
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-toolbar>
+
+    <v-list style="flex: 1;overflow-y: auto; overflow-x:hidden;">
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>dashboard</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-title>仪表盘</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-badge
+                  color="pink"
+                  dot
+                  v-if="primaryDrawer.mini"
+                >
+                  <v-icon>mail</v-icon>
+                </v-badge>
+                <v-icon
+                  v-else
+                >mail</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>
+                询盘
+
+              </v-list-item-title>
+              <div>
+                <v-chip
+                  color="pink"
+                  dark
+                >6</v-chip>
+              </div>
+            </v-list-item>
+      <v-list-group
+        v-for="item in items"
+        :key="item.title"
+        v-model="item.active"
+        no-action
+      >
+
+        <template v-slot:activator>
+              <v-list-item-icon>
+                <v-badge
+                  color="pink"
+                  dot
+                  v-if="primaryDrawer.mini"
+                >
+                  <v-icon v-html="item.action"></v-icon>
+                </v-badge>
+                <v-icon
+                  v-else
+                v-html="item.action"></v-icon>
+              </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+              <v-list-item >
                 <v-list-item-content>
-                  <v-list-item-title>Admin</v-list-item-title>
+                  <v-list-item-title>Test</v-list-item-title>
                 </v-list-item-content>
-              </template>
+                <div>
+                  <v-chip
+                    color="pink"
+                    dark
+                  >6</v-chip>
+                </div>
+             </v-list-item>
 
-              <v-list-item
-                v-for="(admin, i) in admins"
-                :key="i"
-                link
-              >
-                <v-list-item-title v-text="admin[0]"></v-list-item-title>
-                <v-list-item-icon>
-                  <v-icon v-text="admin[1]"></v-icon>
-                </v-list-item-icon>
-              </v-list-item>
-            </v-list-group>
+        <v-list-item
+          v-for="subItem in item.items"
+          :key="subItem.title"
+          @click=""
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>settings</v-icon>
+              </v-list-item-icon>
 
-            <v-list-group
-              sub-group
-              no-action
-            >
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title>Actions</v-list-item-title>
-                </v-list-item-content>
-              </template>
-              <v-list-item
-                v-for="(crud, i) in cruds"
-                :key="i"
-                @click=""
-              >
-                <v-list-item-title v-text="crud[0]"></v-list-item-title>
-                <v-list-item-action>
-                  <v-icon v-text="crud[1]"></v-icon>
-                </v-list-item-action>
-              </v-list-item>
-            </v-list-group>
-          </v-list-group>
-          <v-list-group
-            prepend-icon="account_circle"
-            value="true"
-          >
-            <template v-slot:activator>
-              <v-list-item-title>Users</v-list-item-title>
-            </template>
+              <v-list-item-title>设置</v-list-item-title>
+            </v-list-item>
 
-            <v-list-group
-              no-action
-              sub-group
-              value="true"
-            >
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title>Admin</v-list-item-title>
-                </v-list-item-content>
-              </template>
 
-              <v-list-item
-                v-for="(admin, i) in admins"
-                :key="i"
-                link
-              >
-                <v-list-item-title v-text="admin[0]"></v-list-item-title>
-                <v-list-item-icon>
-                  <v-icon v-text="admin[1]"></v-icon>
-                </v-list-item-icon>
-              </v-list-item>
-            </v-list-group>
-
-            <v-list-group
-              sub-group
-              no-action
-            >
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title>Actions</v-list-item-title>
-                </v-list-item-content>
-              </template>
-              <v-list-item
-                v-for="(crud, i) in cruds"
-                :key="i"
-                @click=""
-              >
-                <v-list-item-title v-text="crud[0]"></v-list-item-title>
-                <v-list-item-action>
-                  <v-icon v-text="crud[1]"></v-icon>
-                </v-list-item-action>
-              </v-list-item>
-            </v-list-group>
-          </v-list-group>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>settings</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-title>设置</v-list-item-title>
-          </v-list-item>
-        </v-list>
-
+    </v-list>
+        
+      </div>
       </v-navigation-drawer>
 
       <v-app-bar
@@ -153,12 +123,15 @@
         color="primary"
         dark
         app
+
       >
         <v-app-bar-nav-icon
           v-if="primaryDrawer.type !== 'permanent'"
           @click.stop="primaryDrawer.model = !primaryDrawer.model"
         />
-        <v-toolbar-title></v-toolbar-title>
+        <v-toolbar-title>
+          Title          
+        </v-toolbar-title>
       </v-app-bar>
 
       <v-content>
@@ -256,7 +229,47 @@
 export default {
   name: 'app',
   data: () => ({
-      admins: [
+        items: [
+          {
+            action: 'local_activity',
+            title: 'Attractions',
+            items: [
+              { title: 'List Item' },
+            ],
+          },
+          {
+            action: 'restaurant',
+            title: 'Dining',
+            active: true,
+            items: [
+              { title: 'Breakfast & brunch' },
+              { title: 'New American' },
+              { title: 'Sushi' },
+            ],
+          },
+          {
+            action: 'school',
+            title: 'Education',
+            items: [
+              { title: 'List Item' },
+            ],
+          },
+          {
+            action: 'directions_run',
+            title: 'Family',
+            items: [
+              { title: 'List Item' },
+            ],
+          },
+          {
+            action: 'healing',
+            title: 'Health',
+            items: [
+              { title: 'List Item' },
+            ],
+          },
+        ],
+        admins: [
         ['Management', 'people_outline'],
         ['Settings', 'settings'],
       ],
@@ -284,6 +297,26 @@ export default {
 <style>
 #app {
 }
+
+/*::-webkit-scrollbar {
+  width: 0.3rem;
+  height: 0.3rem;
+}
+::-webkit-scrollbar-track {
+  border-radius: 0;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 0;
+  background: #535353;
+  transition: all .2s;
+}
+::-webkit-scrollbar-thumb:hover {
+  background-color: #606060;
+}
+
+::-webkit-scrollbar-corner{
+  background: #232323;
+}*/
 
 h1, h2 {
   font-weight: normal;
---
+++ src/components/VularDrawer.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-navigation-drawer
+    :miniVariant = "mini"
+    :bottom = "bottom"
+    :clipped = 'clipped'
+    :disableRouteWatcher = 'disableRouteWatcher'
+    :disableResizeWatcher = 'disableResizeWatcher'
+    :height = 'height'
+    :floating = 'floating'
+    :miniVariantWidth = 'miniVariantWidth'
+    :mobileBreakPoint = 'mobileBreakPoint'
+    :permanent = 'permanent'
+    :right = 'right'
+    :stateless = 'stateless'
+    :temporary = 'temporary'
+    :touchless = 'touchless'
+    :width = 'width'
+    :app = 'app'
+    :dark = 'dark'
+    :light = 'light'
+    v-model="model"
+    >
+    <slot></slot>
+
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  name: 'vular-drawer',
+  props: {
+    bottom: Boolean,
+    clipped: Boolean,
+    disableRouteWatcher: Boolean,
+    disableResizeWatcher: Boolean,
+    height: {
+      type: [Number, String],
+      default: '100%'
+    },
+    floating: Boolean,
+    miniVariant: Boolean,
+    miniVariantWidth: {
+      type: [Number, String],
+      default: 80
+    },
+    mobileBreakPoint: {
+      type: [Number, String],
+      default: 1264
+    },
+    permanent: Boolean,
+    right: Boolean,
+    stateless: Boolean,
+    temporary: Boolean,
+    touchless: Boolean,
+    width: {
+      type: [Number, String],
+      default: 300
+    },
+
+    //mixins/Applicationable
+    app: Boolean,
+
+    //mixins/themable
+    dark: {
+      type: Boolean,
+      default: null
+    },
+    light: {
+      type: Boolean,
+      default: null
+    },
+
+    //mixins/Overlayable
+    hideOverlay: Boolean,
+  },
+  data: function () {
+    return{
+      mini: this.miniVariant,
+      model: true,
+    }    
+  },
+  
+
+  methods: {
+  },
+
+  created () {
+    const self = this;
+    $bus.$on('changeDrawerMini', function () {
+      self.mini = !self.mini;
+      $bus.$emit('miniChanged', self.mini);
+    })
+
+    $bus.$on('vularAction', function (event) {
+      if(event.action == 'hideOrShowDrawer'){
+        self.model = !self.model;
+      }
+    })
+  },
+  
+};
+</script>
