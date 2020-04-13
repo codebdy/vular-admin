b3ce3f874af4cb6c2fcf060ebe53a1cf2e2d00dc
Water.Li <11011968@qq.com>
2020-04-13 20:45:04 +0800

    update

--- src/store.js
+++ src/store.js
@@ -1,64 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import defaultTheme from './themes/default'
 
 Vue.use(Vuex)
+
 
 export default 
 new Vuex.Store({
   state:{
-      vularApp:{
-        fontFamily:"Microsoft YaHei",
-        logo: {
-          src: "/images/logo.png",
-          title:"Vular",
-          alt: "An amazing framework",
-        },
-        drawer: {
-          showLogo: true,
-          color: "#1b1b28",
-          src:"",
-          dark:true,
-          light:false,
-          model: null,
-          type: 'default (no property)',
-          clipped: false,
-          floating: false,
-          mini: false,
-          miniVariantWidth: 70,
-          expandOnHover:false,
-          logo:{
-            color: "#1a1a27",
-            dark: false,
-            flat: true,
-            src:"",
-            style:"border-bottom:#0e0e18 solid 1px;"
-          },
-          fontSize: "0.825rem",
-        },
-        appbar:{
-          showLogo: false,
-          dark: false,
-          light: false,
-          color: "#f4f5fa",
-          src: "",
-          flat:true,
-          style:"border-bottom:#dbdfef solid 1px;"
-        },
-        footer: {
-          inset: true,
-          color:"",
-          dark:false,
-          light:false,
-          src:"",
-        },
-        content:{
-          fontFamily:"",
-          color: "#ecebf2",
-          src: "",
-          flat: true,
-          cardColor:"#f4f5fa",
-        },
-      },
+      vularApp: defaultTheme,
   },
 
  
