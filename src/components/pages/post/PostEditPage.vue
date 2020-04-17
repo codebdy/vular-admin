9081e89926ba446827cea27ea89f6741eb73b903
Water.Li <11011968@qq.com>
2020-04-17 17:48:07 +0800

    Update PostEditPage.vue

--- src/components/pages/post/PostEditPage.vue
+++ src/components/pages/post/PostEditPage.vue
@@ -7,17 +7,17 @@
   >
     <VularEditPageHeader @stick="onStick" :title="'文章编辑'">
     </VularEditPageHeader>
-    <div style="position: relative; flex: 1; border:solid 1px;">
-      <div style="position:absolute; background:url(/images/a1.jpg); top:-10%; left:-10%;width: 120%;height: 500px; background-repeat: no-repeat; background-size: 100% " class="header-image"
-      >
-        <div 
-          style="width: 100%;height: 100%; opacity: 0.7; background-image: linear-gradient(257deg,#b6aaae,#79abb5 71%,#b9b1aa)"
-          >
+      <div style="position: absolute; top:0; left:0; width: 100%; height: 400px;overflow: hidden;">
+        <div style="position:absolute; background:url(/images/a1.jpg); top:-30px; left:-30px;width: 120%;height: 500px; background-repeat: no-repeat; background-size: 100% ;border:solid 1px;" class="header-image"
+        >
+          <div 
+            style="width: 120%;height: 100%; opacity: 0.7; background-image: linear-gradient(257deg,#b6aaae,#79abb5 71%,#b9b1aa)"
+            >
+          </div>
         </div>
       </div>
-      <v-container fluid style="height: 1000px; position: relative; margin-top:300px;background: #FFF">
+      <v-container fluid style="height: 1000px; position: relative; margin-top:500px;background: #FFF">
       </v-container>
-    </div>
   </v-content>
 </template>
 
