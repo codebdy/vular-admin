ee4d1b086df56ec90d2b3b198614cfbcfeb02b62
Water.Li <11011968@qq.com>
2020-04-18 15:11:55 +0800

    Update VularMediaSelect.vue

--- src/components/media/VularMediaSelect.vue
+++ src/components/media/VularMediaSelect.vue
@@ -97,29 +97,14 @@
           </v-row>
         </v-col>
         <v-col
-          v-for="n in 10"
-          :key="n"
           class="d-flex child-flex"
           cols="12"
           md="2"
           style="padding:10px;"
         >
-            <v-img
-              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-              aspect-ratio="1"
-              class="media-thubnail-rounded"
-            >
-              <template v-slot:placeholder>
-                <v-row
-                  class="fill-height ma-0"
-                  align="center"
-                  justify="center"
-                >
-                  <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                </v-row>
-              </template>
-            </v-img>
+          <v-img aspect-ratio="1"" style="border:solid 1px;">
+           
+          </v-img>
         </v-col>
       </v-row>
 
