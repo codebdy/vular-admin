6c40ec5215da1843051f345c2ed6ae2749b34921
Water.Li <11011968@qq.com>
2020-04-21 18:56:32 +0800

    update

--- src/components/tinymce/plugins.js
+++ src/components/tinymce/plugins.js
@@ -2,6 +2,6 @@
 // Detail plugins list see https://www.tinymce.com/docs/plugins/
 // Custom builds see https://www.tinymce.com/download/custom-builds/
 
-const plugins = ['advlist anchor autolink autosave code codesample colorpicker contextmenu directionality emoticons hr imagetools insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print save spellchecker tabfocus table template textcolor textpattern visualblocks visualchars wordcount']
+const plugins = ['advlist anchor autolink autosave code codesample image directionality emoticons hr imagetools insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print save spellchecker tabfocus table template textpattern visualblocks visualchars wordcount']
 
 export default plugins
