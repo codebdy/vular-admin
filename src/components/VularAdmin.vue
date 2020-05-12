e108e275637b56267252c02ed2f7786d24bb4fa3
Water.Li <11011968@qq.com>
2020-05-12 18:05:20 +0800

    remove setting buttons

--- src/components/VularAdmin.vue
+++ src/components/VularAdmin.vue
@@ -51,50 +51,6 @@
       </v-btn>
     </v-snackbar>
 
-    <v-speed-dial
-      v-model="fab"
-      top
-      right
-      open-on-hover
-      transition
-    >
-      <template v-slot:activator>
-        <v-btn
-          v-model="fab"
-          color="blue darken-2"
-          dark
-          fab
-        >
-          <v-icon v-if="fab">mdi-close</v-icon>
-          <v-icon v-else>mdi-account-circle</v-icon>
-        </v-btn>
-      </template>
-      <v-btn
-        fab
-        dark
-        small
-        color="green"
-      >
-        <v-icon>mdi-pencil</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        small
-        color="indigo"
-      >
-        <v-icon>mdi-plus</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        small
-        color="red"
-      >
-        <v-icon>mdi-delete</v-icon>
-      </v-btn>
-    </v-speed-dial>
-
     <AppFab></AppFab>
     <v-footer
       :inset="$store.state.vularApp.footer.inset"
@@ -134,7 +90,6 @@
       },
       successSnackbar:false,
       successMessage:'',
-      fab:false,
     }),
 
     computed:{
