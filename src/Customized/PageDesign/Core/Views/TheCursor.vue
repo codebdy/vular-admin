4e2c23c197a1e130d4036de947d3317b0a234b1c
Water.Li <11011968@qq.com>
2020-05-17 22:15:07 +0800

    cursor can show on vertical

--- src/Customized/PageDesign/Core/Views/TheCursor.vue
+++ src/Customized/PageDesign/Core/Views/TheCursor.vue
@@ -2,8 +2,14 @@
   <div 
     class="the-cursor"
     v-if="isShow"
+    :style="{
+      width:width,
+      top:top,
+      left:left,
+      height:height,
+    }"
+    :class="theClass"
   >
-    Cursor
   </div>
 </template>
 
@@ -16,7 +22,12 @@
     },
 
     data: () => ({
-      isShow:false
+      isShow:false,
+      width:"0",
+      height:'0',
+      top:"0%",
+      left:"0",
+      theClass:"",
     }),
     created(){
       window.$bus.$on('showCursor', this.onShow)
@@ -32,18 +43,73 @@
 
     methods: {
       onShow(event, position){
-        console.log(event)
+        let rect = event.target.getBoundingClientRect()
         if(position == 'in'){
           this.isShow = true
+          //this.theClass = 'vertical'
+          this.left = rect.left + 'px'
+          this.top = (rect.top + rect.height/2) +  'px'
+          this.width = rect.width + 'px'
+          this.height = 4 + 'px'
         }
       },
 
       onHide(){
         this.isShow = false
+        this.theClass = ""
+        //this.width=0
+        //this.height=0
       },
     }
   }
 </script>
 
 <style scoped>
+  .the-cursor{
+    position: fixed;
+    background: #5d78ff;
+    border: #fff solid 1px;
+    z-index: 10020;
+    pointer-events: none;
+    transition: width 0.3s, height 0.3s;
+    will-change: auto;
+  }
+
+  .the-cursor.vertical{
+    transform:rotate(90deg);
+    -ms-transform:rotate(90deg); 	/* IE 9 */
+    -moz-transform:rotate(90deg); 	/* Firefox */
+    -webkit-transform:rotate(90deg); /* Safari 和 Chrome */
+    -o-transform:rotate(90deg); 	/* Opera */    
+  }
+
+  .the-cursor::after{
+    position: absolute;
+    content: '';
+    width: 0; 
+    height: 0;
+    top: 0.15rem;
+    left: calc(50% - 2px);
+    border-width: 4px;
+    border-style: solid;
+    border-color:#5d78ff transparent  transparent  transparent;
+  }
+
+  .the-cursor::before{
+    position: absolute;
+    content: '';
+    width: 0; 
+    height: 0;
+    top: -0.60rem;
+    left: calc(50% - 2px);
+    border-width: 4px;
+    border-style: solid;
+    border-color:transparent transparent #5d78ff  transparent;
+  }
+
+  .the-cursor.vertical::after{
+    top: 0.25rem;
+
+  }
+
 </style>
